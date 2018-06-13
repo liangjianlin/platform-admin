3ffa165cfef26f1da998c555d5e42ad219778f98
liangjianlin <nbaliangjianlin@qq.com>
2018-06-13 22:37:41 +0800

    day4

---
+++ src/main/java/cn/com/ljl/platformadmin/web/AuthController.java
@@ -0,0 +1,24 @@
+package cn.com.ljl.platformadmin.web;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthController {
+
+    @RequestMapping(value = "/login")
+    public JSONObject login() {
+        JSONObject rs = new JSONObject();
+        rs.put("token", "dsfafasfdasfegreg");
+        return rs;
+    }
+
+    @RequestMapping(value = "/user")
+    public JSONObject user() {
+        JSONObject rs = new JSONObject();
+        rs.put("user", "admin");
+        return rs;
+    }
+}
