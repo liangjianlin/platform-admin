ed9d094f0d6c6fb33d0b5a9038ca77b827a5a3b5
liangjianlin <nbaliangjianlin@qq.com>
2018-07-25 23:16:19 +0800

    day9

--- user/src/main/java/cn/com/ljl/user/service/UserServiceImpl.java
+++ user/src/main/java/cn/com/ljl/user/service/UserServiceImpl.java
@@ -4,6 +4,7 @@ import cn.com.ljl.commonservicefacade.service.UserService;
 import com.alibaba.dubbo.config.annotation.Service;
 
 @Service(version = "1.0.0")
+@org.springframework.stereotype.Service
 public class UserServiceImpl implements UserService {
 
     @Override
