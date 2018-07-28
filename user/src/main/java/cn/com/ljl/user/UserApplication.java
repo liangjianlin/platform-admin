e1df2b9108bef28b9f1fbd3d94a23e9435d1cef2
liangjianlin <nbaliangjianlin@qq.com>
2018-07-28 16:04:49 +0800

    day10

--- user/src/main/java/cn/com/ljl/user/UserApplication.java
+++ user/src/main/java/cn/com/ljl/user/UserApplication.java
@@ -2,9 +2,16 @@ package cn.com.ljl.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UserApplication {
+public class UserApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(UserApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
